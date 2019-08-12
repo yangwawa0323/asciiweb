f5e0c18b8fa87c0a0c601986fef8360208ba721f
yangwawa0323 <yangwawa0323@163.com>
2019-08-13 02:51:05 +0800

    Add files via upload
    template.html change theme to "tango"
    bootstrap.py complatible with python 3.

--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
-from BaseHTTPServer import *
+import sys
+print(sys.version_info)
+#######################################
+# from python 3 HTTPServer has been move to http.server
+# the following code is import library from different version
+# of python.
+# determine the python version code
+if sys.version_info[0] > 2:
+    from http.server import *
+else:
+    from BaseHTTPServer import *
+    
+#######################################
 import os
 import re
 
@@ -9,13 +21,15 @@ VIDEO_PATH='video'
 class ASCII_Player(BaseHTTPRequestHandler):
     
     def do_GET(self):
-        print self.path
         m = re.match(r'(?P<filepath>/' + VIDEO_PATH + '/.*)\?play',self.path)
         if m:
             self.send_response(200)
             self.end_headers()
             play_file = m.groupdict()['filepath']
-            self.wfile.write( self.format_player(play_file))
+            if sys.version_info[0] > 2:
+                self.wfile.write( self.format_player(play_file).encode())
+            else:
+                self.wfile.write( self.format_player(play_file))
             return
 
         n = re.match(r'/$', self.path)
@@ -31,27 +45,31 @@ class ASCII_Player(BaseHTTPRequestHandler):
                 else:
                     self.send_response(404)
             self.end_headers()
-            self.wfile.write(response)
+            if sys.version_info[0] > 2:
+                self.wfile.write(response.encode())
+            else:
+                self.wfile.write(response)
             return
         j = re.match(r'/(?P<filepath>.*\..*)$', self.path)
         if j:
             path = os.path.join(os.path.dirname(os.path.realpath(__file__)), j.groupdict()['filepath'])
-            print "absolute path: {0}".format(path)
-            # print "__file__: {0}".format(__file__)
-
             if os.path.exists(path):
                 with open(path) as f:
                     response = f.read()
-                    self.send_response(200)
-                    self.end_headers()
-                    
-                    self.wfile.write(response)
+                    try:
+                        self.end_headers()
+                    except:
+                        pass
+                    if sys.version_info[0] > 2:
+                        self.wfile.write(response.encode())
+                    else:
+                        self.wfile.write(response)
+
             else:
                 self.send_response(404)
 
     def search(self):
         path = os.path.join(os.path.dirname(os.path.realpath(__file__)),VIDEO_PATH)
-    
         if os.path.exists(path):
             filelist = os.listdir(path)
             castlist = filter(lambda f: f.endswith('.cast'), filelist)
@@ -64,7 +82,7 @@ class ASCII_Player(BaseHTTPRequestHandler):
             return re.sub(r'@@VIDEO@@', path, content)
 
 
-print "Open browser and type \"http://localhost:8080/\""
+print("Open browser and type \"http://localhost:8080/\"")
 server_address = ('', 8080)
 httpd = HTTPServer(server_address,ASCII_Player)
 httpd.serve_forever()
